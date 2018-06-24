c688a7b980312293a577d7724abc5a6e7d2e8ec1
nmatos46 <ncm46@cornell.edu>
2018-06-24 18:16:56 -0400

    fix: Errors thrown on hard reload removed - Change window.initMap such that markers are   added only when restaurants have been   fetched from the data

--- js/main.js
+++ js/main.js
@@ -76,14 +76,17 @@ window.initMap = () => {
     lat: 40.722216,
     lng: -73.987501
   };
-  
+
   self.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: loc,
     scrollwheel: false
   });
 
-  addMarkersToMap();
+  if (self.restaurants){
+    //only load markers if restuarants have been defined
+    addMarkersToMap();
+  }
 }
 
 /**
