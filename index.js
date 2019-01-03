6483efc8d19ca3740be09ec8b93d0a89badd784c
nmatos46 <ncm46@cornell.edu>
2019-01-03 17:29:16 -0500

    feat: add error handling to index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var file = new static.Server();
 
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 9000);
+    file.serve(request, response, function (err, result) {
+      //Citation:https://www.npmjs.com/package/node-static
+      //Add error handling
+      if (err) { // There was an error serving the file
+          console.error("Error serving " + request.url + " - " + err.message);
+
+          // Respond to the client
+          response.writeHead(err.status, err.headers);
+          response.end();
+      };
+    }).resume();
+  });  
+}).listen(process.env.PORT || 5000);
