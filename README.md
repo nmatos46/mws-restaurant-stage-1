7f76ae874271780525a7058319ad72b7b3dc7f1f
nmatos46 <ncm46@cornell.edu>
2019-01-04 00:03:03 -0500

    feat: Express server using node.js

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 ---
 #### _Three Stage Course Material Project - Restaurant Reviews_
 
-## Project Overview: Stage 1
+
+
+****************************Original Udacity Readme***********************************
+## Udacity Project Overview: Stage 1
 
 For the **Restaurant Reviews** projects, you will incrementally convert a static webpage to a mobile-ready web application. In **Stage One**, you will take a static design that lacks accessibility and convert the design to be responsive on different sized displays and accessible for screen reader use. You will also add a service worker to begin the process of creating a seamless offline experience for your users.
 
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-//Citation: https://medium.com/shovel-apps/simple-node-js-server-on-heroku-210ec24f485
+//Citation: https://medium.com/jondengdevelops/deploy-your-front-end-app-in-20-lines-of-code-24be44f8b51
 
 /**Create a simple http server using node.js */
-var static = require('node-static');
-var file = new static.Server();
+var express = require('express');
+var app = express();
+var path = require('path');
 
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    console.log(request.url)
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 9000);
+app.use(express.static(path.join(__dirname)));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/imgMedium", express.static(__dirname + '/imgMedium'));
+app.use("/imgSmall", express.static(__dirname + '/imgSmall'));
+app.use("/js", express.static(__dirname + '/js'));
+
+// viewed at based directory http://localhost:9000/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+// add other routes below
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname + 'views/about.html'));
+});
+
+app.listen(process.env.PORT || 9000);
